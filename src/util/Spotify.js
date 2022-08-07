599b07a68426eeebea967955130136ba769c16e6
Caleb <80975506+CAHernandez7@users.noreply.github.com>
2022-08-07 10:58:17 -0700

    changed redirect site to online website

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -55,9 +55,13 @@ const Spotify = {
         const headers = { Authorization: `Bearer ${accessToken}`};
         let userId;
 
-        return fetch('https://api.spotify.com/v1/me', {headers: headers}).then(response => response.json).then(jsonResponse => {
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}
+        ).then(response => response.json()
+            ).then(jsonResponse => {
             userId = jsonResponse.id;
-            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {headers: headers, method: 'POST', body: JSON.stringify({name: name})}).then(response => response.json()).then(jsonResponse => {
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {headers: headers, method: 'POST', body: JSON.stringify({name: name})}
+            ).then(response => response.json()
+            ).then(jsonResponse => {
                 const playlistId = jsonResponse.id;
                 return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}}/tracks`, {
                     headers: headers,
